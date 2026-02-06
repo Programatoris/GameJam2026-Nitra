cc64fc01972fdf4a8af2ddd10746ebfe04f3d283
Patrik Šabo <sabo.patrik1@gmail.com>
2026-02-06 12:03:44 +0100

    added in game pause menu, fixed bug after going back to menu and not starting the level

--- main/walk-in-the-park/Scripts/SoundManager.gd
+++ main/walk-in-the-park/Scripts/SoundManager.gd
@@ -3,28 +3,22 @@ extends Node
 
 #================================================================================
 # Variables
-var effects_nodes = getRoot().get_nodes_in_group("Effects")
-var music_nodes = getRoot().get_nodes_in_group("Music")
+var effects_nodes: Array[Node]
+var music_nodes: Array[Node]
 
 
 #================================================================================
 # Functions
-func _ready() -> void:
-	print(music_nodes)
-	print(effects_nodes)
-
-
-@warning_ignore("unused_parameter")
-func _process(delta: float) -> void:
-	pass
-
-
 func musicGroup() -> void:
 	var volume_db = convertPercentToDecibels(Data.data["settings"]["Music"])
 	
 	for node in get_tree().get_nodes_in_group("Music"):
 		if node is AudioStreamPlayer or node is AudioStreamPlayer2D or node is AudioStreamPlayer3D:
-			node.volume_db = volume_db
+			if Data.data["settings"]["Music"] == 0.0:
+				node.stream_paused = true
+			else:
+				node.stream_paused = false
+				node.volume_db = volume_db
 
 
 func effectsGroups() -> void:
@@ -32,15 +26,15 @@ func effectsGroups() -> void:
 	
 	for node in get_tree().get_nodes_in_group("Effects"):
 		if node is AudioStreamPlayer or node is AudioStreamPlayer2D or node is AudioStreamPlayer3D:
-			node.volume_db = volume_db
+			if Data.data["settings"]["Effects"] == 0.0:
+				node.stream_paused = true
+			else:
+				node.stream_paused = false
+				node.volume_db = volume_db
 
 
 #==================================================
 # Helper functions
-func getRoot() -> MainLoop:
-	return Engine.get_main_loop()
-
-
 func convertPercentToDecibels(_percent) -> int:
 	var percent = clamp(_percent, 0, 100)
-	return lerp(-80, 0, percent / 100.0)
+	return lerp(-15, 0, percent / 100.0)
