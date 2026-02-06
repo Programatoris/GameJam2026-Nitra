c1c1bc5b9850f3a97c092256fe5bcfcc3a3bb72a
PalcivaPapricka <viktor.kramar3256@gmail.com>
2026-02-06 09:32:32 +0100

    asfsf
    sefsef

--- main/walk-in-the-park/Nodes/jump_boost.gd
+++ main/walk-in-the-park/Nodes/jump_boost.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var detection_area: Area2D = $Area2D
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sprite: Sprite2D = $Sprite2D
 
 var is_active := true
 
---
+++ main/walk-in-the-park/Scripts/player2.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -225.0
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D2
+
+var is_dead := false
+var can_double_jump := false # Premenná, ktorú mení pickup
+
+func _ready() -> void:
+	add_to_group("player")
+	sprite.play("default")
+
+
+
+func _physics_process(delta: float) -> void:
+	if is_dead:
+		return
+	
+	# Gravitácia
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	# Jump logika
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			# Normálny skok zo zeme
+			velocity.y = JUMP_VELOCITY
+		elif can_double_jump:
+			# Extra skok vo vzduchu z pickupu
+			velocity.y = JUMP_VELOCITY - 100
+			can_double_jump = false # Bonus sa po použití minie
+
+	# Movement
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction != 0:
+		velocity.x = direction * SPEED
+		
+		sprite.flip_h = direction < 0
+		
+		if sprite.animation != "pohyb":
+			pass
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		if sprite.animation != "default":
+			sprite.play("default")
+	
+	move_and_slide()
